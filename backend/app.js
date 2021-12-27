ac54c157991ffeb20265f78f49b050872854ee09
Canna Dntziger <channada@gmail.com>
2021-12-27 23:00:57 +0200

    install mongoose and express

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,27 @@
-console.log("hi   !");
+const express = require('express' )
+const app = express()
+const port = 3500
+
+const mongoose = require('mongoose');
+
+
+
+//const usersRouter = require('./routes/usersRoute');
+app.use(express.json());
+
+//app.use('/api/user',usersRouter);
+
+
+mongoose.connect('mongodb://localhost:27017/memories', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+})
+.then(console.log("connect to DB memories!"));
+
+mongoose.set('debug','true');
+
+
+app.listen(port,()=>{
+    console.log(`example app listening at http://localhost:${port}`)
+})
