eb15b20a84673774b5f01348bb80d3ab4f959951
Canna Dntziger <channada@gmail.com>
2021-12-29 23:40:28 +0200

    add public dir add got app run

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
+
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">ברוכים הבאים לפרוייקט 
+ 
+  </div>;
 }
 
 export default App;
