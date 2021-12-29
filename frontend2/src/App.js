837f40927a3f66a57c728c8a754458f8317465d2
Canna Dntziger <channada@gmail.com>
2021-12-30 00:11:03 +0200

    add upload conponnent

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,11 +1,12 @@
 
 import "./App.css";
+import Upload from "./components/upload";
 
 
 
 function App() {
   return <div className="App">ברוכים הבאים לפרוייקט 
- 
+ <Upload></Upload>
   </div>;
 }
 
