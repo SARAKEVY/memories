44476d99420e434f6b972bf00859b67a32ef5f3c
Canna Dntziger <channada@gmail.com>
2022-01-03 20:14:37 +0200

    add select-react

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -3,6 +3,7 @@ import './sass/main.css';
 import "./App.css";
 import React from "react";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Item from "./components/item";
 //import Upload from "./components/upload";
 //import Signup from "./components/signup";
 function App() {
@@ -11,6 +12,7 @@ function App() {
     
   <div className="App">
     <Account></Account>
+    <Item></Item>
     {/* <Routes>
       <Route path = "/upload" component={Upload}/>
       <Route path = "/signup" component={Signup}/>
