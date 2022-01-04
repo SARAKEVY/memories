a784774285bee7b8719583c90db1ee05729d6156
nechami falk <falknechami@gmail.com>
2022-01-05 00:24:40 +0200

    add routing

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,29 +1,33 @@
 import Account from "./components/account";
 import './sass/main.css';
 import React from "react";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route } from "react-router-dom";
 import Item from "./components/item";
-//import Upload from "./components/upload";
+import ImageUpload from "./components/imageUpload";
 import Signup from "./components/signup";
 import FormikFormDemo from "./components/login";
+import './App.css';
 
 
 
 function App() {
-  return (
-    
-    
-  <div className="App">
-    <FormikFormDemo></FormikFormDemo>
-    <Account></Account>
-    <Signup></Signup> 
-    {/* <Routes>
-      <Route path = "/upload" component={Upload}/>
-      <Route path = "/signup" component={Signup}/>
-      <Route path = "/account" component={Account}/>
-    </Routes> */}
-  </div>
+
+
   
-  )}
+  return (
+    <BrowserRouter>
+    
+      
+    
+    <Routes>
+      <Route path = "item" element={<Item/>}/>
+      <Route path = "signup" element={<Signup/>} />
+      <Route path = "account" element={<Account/>}/>
+    </Routes> 
+  
+    
+    </BrowserRouter>
+  )};
 
 export default App;
--- frontend2/src/components/login.js
+++ frontend2/src/components/login.js
@@ -1,6 +1,5 @@
-
 import React, { useEffect, useState } from 'react';
-import { useFormik } from 'formik';
+import { useForm, Controller } from 'react-hook-form';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
@@ -10,62 +9,36 @@ import { Checkbox } from 'primereact/checkbox';
 import { Dialog } from 'primereact/dialog';
 import { Divider } from 'primereact/divider';
 import { classNames } from 'primereact/utils';
-import { CountryService } from '../service/CountryService';
-import './FormDemo.css';
 
-const FormikFormDemo = () => {
-    const [countries, setCountries] = useState([]);
+//import './FormDemo.css';
+
+const ReactHookFormDemo = () => {
+    
     const [showMessage, setShowMessage] = useState(false);
     const [formData, setFormData] = useState({});
-    const countryservice = new CountryService();
 
-    useEffect(() => {
-        countryservice.getCountries().then(data => setCountries(data));
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    const defaultValues = {
+        name: '',
+        email: '',
+        password: '',
+        date: null,
+        country: null,
+        accept: false
+    }
 
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            password: '',
-            date: null,
-            country: null,
-            accept: false
-        },
-        validate: (data) => {
-            let errors = {};
+   
 
-            if (!data.name) {
-                errors.name = 'Name is required.';
-            }
+    const { control, formState: { errors }, handleSubmit, reset } = useForm({ defaultValues });
 
-            if (!data.email) {
-                errors.email = 'Email is required.';
-            }
-            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
-                errors.email = 'Invalid email address. E.g. example@email.com';
-            }
-            if (!data.password) {
-                errors.password = 'Password is required.';
-            }
+    const onSubmit = (data) => {
+        setFormData(data);
+        setShowMessage(true);
 
-            if (!data.accept) {
-                errors.accept = 'You need to agree to the terms and conditions.';
-            }
+        reset();
+    };
 
-            return errors;
-        },
-        onSubmit: (data) => {
-            setFormData(data);
-            setShowMessage(true);
-
-            formik.resetForm();
-        }
-    });
-
-    const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
     const getFormErrorMessage = (name) => {
-        return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
+        return errors[name] && <small className="p-error">{errors[name].message}</small>
     };
 
     const dialogFooter = <div className="p-d-flex p-jc-center"><Button label="OK" className="p-button-text" autoFocus onClick={() => setShowMessage(false)} /></div>;
@@ -98,45 +71,51 @@ const FormikFormDemo = () => {
             <div className="p-d-flex p-jc-center">
                 <div className="card">
                     <h5 className="p-text-center">Register</h5>
-                    <form onSubmit={formik.handleSubmit} className="p-fluid">
+                    <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
                         <div className="p-field">
                             <span className="p-float-label">
-                                <InputText id="name" name="name" value={formik.values.name} onChange={formik.handleChange} autoFocus className={classNames({ 'p-invalid': isFormFieldValid('name') })} />
-                                <label htmlFor="name" className={classNames({ 'p-error': isFormFieldValid('name') })}>Name*</label>
+                                <Controller name="name" control={control} rules={{ required: 'Name is required.' }} render={({ field, fieldState }) => (
+                                    <InputText id={field.name} {...field} autoFocus className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="name" className={classNames({ 'p-error': errors.name })}>Name*</label>
                             </span>
                             {getFormErrorMessage('name')}
                         </div>
                         <div className="p-field">
                             <span className="p-float-label p-input-icon-right">
                                 <i className="pi pi-envelope" />
-                                <InputText id="email" name="email" value={formik.values.email} onChange={formik.handleChange} className={classNames({ 'p-invalid': isFormFieldValid('email') })} />
-                                <label htmlFor="email" className={classNames({ 'p-error': isFormFieldValid('email') })}>Email*</label>
+                                <Controller name="email" control={control}
+                                    rules={{ required: 'Email is required.', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: 'Invalid email address. E.g. example@email.com' }}}
+                                    render={({ field, fieldState }) => (
+                                        <InputText id={field.name} {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                                <label htmlFor="email" className={classNames({ 'p-error': !!errors.email })}>Email*</label>
                             </span>
                             {getFormErrorMessage('email')}
                         </div>
                         <div className="p-field">
                             <span className="p-float-label">
-                                <Password id="password" name="password" value={formik.values.password} onChange={formik.handleChange} toggleMask
-                                    className={classNames({ 'p-invalid': isFormFieldValid('password') })} header={passwordHeader} footer={passwordFooter} />
-                                <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid('password') })}>Password*</label>
+                                <Controller name="password" control={control} rules={{ required: 'Password is required.' }} render={({ field, fieldState }) => (
+                                    <Password id={field.name} {...field} toggleMask className={classNames({ 'p-invalid': fieldState.invalid })} header={passwordHeader} footer={passwordFooter} />
+                                )} />
+                                <label htmlFor="password" className={classNames({ 'p-error': errors.password })}>Password*</label>
                             </span>
                             {getFormErrorMessage('password')}
                         </div>
                         <div className="p-field">
                             <span className="p-float-label">
-                                <Calendar id="date" name="date" value={formik.values.date} onChange={formik.handleChange} dateFormat="dd/mm/yy" mask="99/99/9999" showIcon />
+                                <Controller name="date" control={control} render={({ field }) => (
+                                    <Calendar id={field.name} value={field.value} onChange={(e) => field.onChange(e.value)} dateFormat="dd/mm/yy" mask="99/99/9999" showIcon />
+                                )} />
                                 <label htmlFor="date">Birthday</label>
                             </span>
                         </div>
-                        <div className="p-field">
-                            <span className="p-float-label">
-                                <Dropdown id="country" name="country" value={formik.values.country} onChange={formik.handleChange} options={countries} optionLabel="name" />
-                                <label htmlFor="country">Country</label>
-                            </span>
-                        </div>
+                        
                         <div className="p-field-checkbox">
-                            <Checkbox inputId="accept" name="accept" checked={formik.values.accept} onChange={formik.handleChange} className={classNames({ 'p-invalid': isFormFieldValid('accept') })} />
-                            <label htmlFor="accept" className={classNames({ 'p-error': isFormFieldValid('accept') })}>I agree to the terms and conditions*</label>
+                            <Controller name="accept" control={control} rules={{ required: true }} render={({ field, fieldState }) => (
+                                <Checkbox inputId={field.name} onChange={(e) => field.onChange(e.checked)} checked={field.value} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                            )} />
+                            <label htmlFor="accept" className={classNames({ 'p-error': errors.accept })}>I agree to the terms and conditions*</label>
                         </div>
 
                         <Button type="submit" label="Submit" className="p-mt-2" />
@@ -146,5 +125,4 @@ const FormikFormDemo = () => {
         </div>
     );
 }
-
-export default FormikFormDemo;
+export default ReactHookFormDemo ;
