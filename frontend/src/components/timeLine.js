f3afb70632f904ae420c5d843bc1e01e88c164cd
sarah <sarah.levy9124@gmail.com>
2022-01-30 21:22:17 +0200

    timeline-axios

--- frontend/src/components/timeLine.js
+++ frontend/src/components/timeLine.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
+import axios from 'axios';
+// import { useHistory } from "react-router-dom"
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import events from "./events.json";
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+
+import { Button } from 'primereact/button';
 
 
 export default function TimeLine() {
+  // const history = useHistory();
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get(`http://localhost:3500/api/items`)
+      .then(function (response) {
+        setItems(response.data)
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+
+  }, [])
 
   return (
     <div
@@ -14,19 +35,29 @@ export default function TimeLine() {
     >
       <h1>CDTN </h1>
       <VerticalTimeline>
-        {events.map(event => (
+
+        {items.map(item => (
           <VerticalTimelineElement
+            key={item.id}
             className="vertical-timeline-element--education"
-            date={event.date}
+            date={item.date}
             iconStyle={{ background: "rgb(13, 150, 243)", color: "#fff" }}
           >
-            <h3
+            <h2
               className="vertical-timeline-element-title"
-              dangerouslySetInnerHTML={{ __html: event.event }}
+              dangerouslySetInnerHTML={{ __html: item.title }} 
+              />
+            <h6
+              className="vertical-timeline-element-subtitle"
+              dangerouslySetInnerHTML={{ __html: item.description }}
+            /* <img src={item.image[1]}></img> */
+
             />
+            <Button > More</Button>
           </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
+
     </div>
   );
 }
