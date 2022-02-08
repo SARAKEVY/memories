f3dc71ee149e2477096a18c54b4654004cd11f81
nechami falk <falknechami@gmail.com>
2022-02-08 13:48:58 +0200

    fix signup

--- frontend/src/components/email.js
+++ frontend/src/components/email.js
@@ -1,29 +1,41 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
 
-export const ContactUs = () => {
-  const form = useRef();
+function Email( props ){
 
-  const sendEmail = (e) => {
-    e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+
+var templateParams = {
+    name: 'James',
+    notes: 'Check this out!'
 };
+
+
+sendEmail = () => {
+   
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    <body>
+    <h1>hello</h1>
+    </body>
+    </html>`;
+
+   
+
+
+
+emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+}
+export default Email;
